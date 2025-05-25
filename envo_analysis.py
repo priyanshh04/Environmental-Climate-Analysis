48df9e8e350d4070180e3b341a5702a25ac7be52
Priyansh <priyanshgangwar05@gmail.com>
2025-05-26 02:41:33 +0530

    Update envo_analysis.py

--- envo_analysis.py
+++ envo_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sn
 from sklearn.preprocessing import MinMaxScaler
 
 # Load Dataset
